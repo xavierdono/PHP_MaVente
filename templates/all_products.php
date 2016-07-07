f3a25bb527a60e269ab6939f13707024a9bdfc2a
Xavier DONO <xavier.ratzel@free.fr>
2016-07-07 20:53:48 +0000

    Refractor header, footer and menu

---
+++ templates/all_products.php
@@ -0,0 +1,8 @@
+<div class="center_content">
+    <div class="center_title_bar">Products</div>
+    <div class="prod_box_big">
+        <div class="center_prod_box_big">
+            Products
+        </div>
+    </div>
+</div>
---
+++ templates/contact.php
@@ -0,0 +1,30 @@
+<div class="center_content">
+    <div class="center_title_bar">Contact Us</div>
+    <div class="prod_box_big">
+        <div class="center_prod_box_big">
+            <div class="contact_form">
+                <div class="form_row">
+                    <label class="contact"><strong>Name:</strong></label>
+                    <input type="text" class="contact_input" />
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Email:</strong></label>
+                    <input type="text" class="contact_input" />
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Phone:</strong></label>
+                    <input type="text" class="contact_input" />
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Company:</strong></label>
+                    <input type="text" class="contact_input" />
+                </div>
+                <div class="form_row">
+                    <label class="contact"><strong>Message:</strong></label>
+                    <textarea class="contact_textarea" ></textarea>
+                </div>
+                <div class="form_row"> <a href="#" class="prod_details">send</a> </div>
+            </div>
+        </div>
+    </div>
+</div>
